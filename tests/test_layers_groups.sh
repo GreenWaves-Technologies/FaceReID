ee419cc5b667fdffc0de2e93dbf199a9b24d25ac
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2020-03-11 09:50:08 +0300

    Enabled testing of silent mode without printf.

--- tests/test_layers_groups.sh
+++ tests/test_layers_groups.sh
@@ -47,7 +47,7 @@ if [ ${#stop_macros[@]} -ne ${#layer_outputs[@]} ]; then
     exit 1
 fi
 
-MAKEFILE_NAME="Makefile"
+MAKEFILE_OPTIONS="-f Makefile $@"
 
 mkdir -p groups_logs
 echo "Generating Model"
@@ -65,9 +65,9 @@ status=0
 for i in $(seq 0 $layers_count)
 do
     echo "Stop word $i: ${stop_macros[$i]} => ${layer_outputs[$i]}"
-    make -f $MAKEFILE_NAME clean > /dev/null 2>&1
-    make -f $MAKEFILE_NAME -j4 CONTROL_MACRO="${stop_macros[$i]}" tiler_models > ../groups_logs/$i.stdout.log 2>&1
-    make -f $MAKEFILE_NAME -j4 CONTROL_MACRO="${stop_macros[$i]}" run >> ../groups_logs/$i.stdout.log 2>&1
+    make $MAKEFILE_OPTIONS clean > /dev/null 2>&1
+    make $MAKEFILE_OPTIONS -j4 CONTROL_MACRO="${stop_macros[$i]}" tiler_models > ../groups_logs/$i.stdout.log 2>&1
+    make $MAKEFILE_OPTIONS -j4 CONTROL_MACRO="${stop_macros[$i]}" run >> ../groups_logs/$i.stdout.log 2>&1
     echo -n "${stop_macros[$i]}; ${layer_outputs[$i]}; " >> ../group_layer_test_summary.csv
     ../../scripts/compareWithBin.py ../activations_dump/${layer_outputs[$i]} ./output.bin >> ../group_layer_test_summary.csv
     if [ $? -ne 0 ]; then
